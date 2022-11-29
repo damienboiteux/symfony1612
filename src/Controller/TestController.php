244329bd94b906a5b165e0b4e10fda62c3c3da00
Damien Boiteux <contact@damienboiteux.com>
2022-11-29 15:47:23 +0100

    twig

---
+++ src/Controller/TestController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController extends AbstractController
+{
+    #[Route('/test', name: 'app_test')]
+    public function index(): Response
+    {
+
+        // logique 
+        // accès BDD
+        // calcul
+
+
+        return $this->render('test/index.html.twig', [
+            'controller_name' => 'TestController',
+            'test' => 'Une valeur',
+        ]);
+    }
+}

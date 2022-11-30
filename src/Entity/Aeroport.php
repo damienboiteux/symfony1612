6c339336b465bba7a5f6500ed792b7ac2e50b909
Damien Boiteux <contact@damienboiteux.com>
2022-11-30 16:32:11 +0100

    relations

---
+++ src/Entity/Aeroport.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AeroportRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AeroportRepository::class)]
+class Aeroport
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 32)]
+    private ?string $nom = null;
+
+    #[ORM\Column]
+    private ?int $nb_pistes = null;
+
+    #[ORM\ManyToOne(inversedBy: 'aeroports')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Ville $ville = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getNbPistes(): ?int
+    {
+        return $this->nb_pistes;
+    }
+
+    public function setNbPistes(int $nb_pistes): self
+    {
+        $this->nb_pistes = $nb_pistes;
+
+        return $this;
+    }
+
+    public function getVille(): ?Ville
+    {
+        return $this->ville;
+    }
+
+    public function setVille(?Ville $ville): self
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+}
--- src/Entity/Ville.php
+++ src/Entity/Ville.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\VilleRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: VilleRepository::class)]
@@ -21,6 +23,14 @@ class Ville
 
     #[ORM\Column]
     private ?int $population = null;
+
+    #[ORM\OneToMany(mappedBy: 'ville', targetEntity: Aeroport::class)]
+    private Collection $aeroports;
+
+    public function __construct()
+    {
+        $this->aeroports = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -59,6 +69,36 @@ class Ville
     public function setPopulation(int $population): self
     {
         $this->population = $population;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Aeroport>
+     */
+    public function getAeroports(): Collection
+    {
+        return $this->aeroports;
+    }
+
+    public function addAeroport(Aeroport $aeroport): self
+    {
+        if (!$this->aeroports->contains($aeroport)) {
+            $this->aeroports->add($aeroport);
+            $aeroport->setVille($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAeroport(Aeroport $aeroport): self
+    {
+        if ($this->aeroports->removeElement($aeroport)) {
+            // set the owning side to null (unless already changed)
+            if ($aeroport->getVille() === $this) {
+                $aeroport->setVille(null);
+            }
+        }
 
         return $this;
     }

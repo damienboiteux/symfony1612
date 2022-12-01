4b632d73ea8b09cdcfae5813083d92131b7ad751
Damien Boiteux <contact@damienboiteux.com>
2022-12-01 15:10:48 +0100

    form

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactType;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'app_contact')]
+    public function index(): Response
+    {
+        $form = $this->createForm(ContactType::class);
+
+        return $this->render('contact/index.html.twig', [
+            'formulaire' => $form->createView(),
+        ]);
+    }
+}

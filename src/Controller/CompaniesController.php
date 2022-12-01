a7ff068055df3083d5de8a69d7745cc27c832b82
Damien Boiteux <contact@damienboiteux.com>
2022-12-01 13:46:14 +0100

    show

--- src/Controller/CompaniesController.php
+++ src/Controller/CompaniesController.php
@@ -2,21 +2,33 @@
 
 namespace App\Controller;
 
+use App\Repository\VilleRepository;
 use App\Repository\CompanyRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CompaniesController extends AbstractController
 {
     #[Route('/companies', name: 'app_companies')]
-    public function index(CompanyRepository $companyRepository): Response
+    public function index(CompanyRepository $repo): Response
     {
-
-        dd($companyRepository->findAll());
+        $companies = $repo->findAll();
 
         return $this->render('companies/index.html.twig', [
-            'controller_name' => 'CompaniesController',
+            'companies' => $companies,
+        ]);
+    }
+
+    #[Route('/companies/{id}', name: "show_company")]
+    public function show(Request $request, CompanyRepository $repo): Response
+    {
+        $id = $request->attributes->get('id');
+        $company = $repo->find($id);
+
+        return $this->render('companies/show.html.twig', [
+            'company' => $company,
         ]);
     }
 }

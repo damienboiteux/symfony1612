4d419882bff1da427bc8466d7ea179348909d9a9
Damien Boiteux <contact@damienboiteux.com>
2022-12-01 16:23:08 +0100

    jeudi form companies

--- src/Controller/CompaniesController.php
+++ src/Controller/CompaniesController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Company;
+use App\Form\CompanyType;
 use App\Repository\CompanyRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,17 +15,29 @@ class CompaniesController extends AbstractController
     public function index(CompanyRepository $repo): Response
     {
         $companies = $repo->findAll();
+        $formulaire = $this->createForm(CompanyType::class);
 
         return $this->render('companies/index.html.twig', [
             'companies' => $companies,
         ]);
     }
 
-    #[Route('/companies/{id}', name: 'show_company')]
+
+    #[Route('/companies/{id}', name: 'show_company', requirements: ['id' => '\d+'])]
     public function show(Company $company): Response
     {
         return $this->render('companies/show.html.twig', [
             'company' =>  $company,
+        ]);
+    }
+
+    #[Route('/companies/create', name: "add_company")]
+    public function add(): Response
+    {
+        $formulaire = $this->createForm(CompanyType::class);
+
+        return $this->render('companies/add.html.twig', [
+            'formulaire'    =>  $formulaire->createView(),
         ]);
     }
 }
--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -3,6 +3,10 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -11,11 +15,18 @@ class ContactType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('sujet')
-            ->add('email')
-            ->add('nom')
-            ->add('message')
-            ->add('submit');
+            ->add('object', TextType::class, [
+                'label'     => 'Sujet',
+                'required'  => false,
+                'help'      => 'Texte d\'aide',
+                'attr'      => [
+                    'placeholder' => 'L\'objet du message',
+                ]
+            ])
+            ->add('email', EmailType::class)
+            ->add('name', TextType::class)
+            ->add('message', TextareaType::class)
+            ->add('submit', SubmitType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
